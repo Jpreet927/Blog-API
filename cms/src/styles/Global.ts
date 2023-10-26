101b55dbb4c93ba057d425fec49c5abda7c18eb3
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2023-10-26 19:40:58 -0400

    refactor: :art: merged global styles into /styles

---
+++ cms/src/styles/Global.ts
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Lato&family=Playfair+Display:ital,wght@0,400;0,700;1,400&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap');
+
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+        font-family: "Playfair Display", serif;
+    }
+
+    a {
+        color: #3F3F3F;
+    }
+`;
+
+export default GlobalStyles;
