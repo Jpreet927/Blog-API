e96091aaf6f3b2875b0eba68b4e1b50fe5f09f86
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2023-10-25 20:13:11 -0400

    feat: :sparkles: created cms routes, gaurded routes from unauthenticated users

--- cms/src/App.tsx
+++ cms/src/App.tsx
@@ -1,14 +1,44 @@
-import React, { useContext, useEffect } from "react";
+import React from "react";
 import "./App.css";
-import UserProvider, { UserContext } from "./context/AuthContext";
+import UserProvider from "./context/AuthContext";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import PostPage from "./pages/PostPage";
+import ApplicationsPage from "./pages/ApplicationsPage";
+import LoginPage from "./pages/LoginPage";
+import PrivateRoute from "./components/Utils/PrivateRoute";
 
 function App() {
-    const { user } = useContext(UserContext);
-
     return (
         <UserProvider>
             <div className="App">
-                <header className="App-header"></header>
+                <Routes>
+                    <Route
+                        path="/"
+                        element={
+                            <PrivateRoute>
+                                <HomePage />
+                            </PrivateRoute>
+                        }
+                    />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route
+                        path="/post/:postid"
+                        element={
+                            <PrivateRoute>
+                                <PostPage />
+                            </PrivateRoute>
+                        }
+                    />
+                    <Route
+                        path="/applications"
+                        element={
+                            <PrivateRoute>
+                                <ApplicationsPage />
+                            </PrivateRoute>
+                        }
+                    />
+                </Routes>
             </div>
         </UserProvider>
     );
